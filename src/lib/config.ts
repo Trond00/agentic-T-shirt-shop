acb8ce7b03b4096c2fa5911e2e8dff206150a34c
Trond00 <trond.slinning@gmail.com>
2025-11-25 12:29:13 +0100

    Added env and fallback on config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,8 @@
 export const config = {
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
-  apiURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 
+           process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 
+           "http://localhost:3000",
+  apiURL: process.env.NEXT_PUBLIC_API_URL || 
+          process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api` : 
+          "http://localhost:3000/api",
 };
